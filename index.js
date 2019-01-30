7fb61e7776d4b0275de90787c812a4c2cfc63339
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-01-30 15:50:45 +0800

    fix cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
 const BodyParser = require('koa-bodyparser')
 const router = require('./router/blog-router')
+const cors = require('@koa/cors')
 
 const app = new Koa()
 
+app.use(cors({
+    origin: 'http://localhost:8000'
+}))
 app.use(BodyParser())
 app.use(router.routes())
 
