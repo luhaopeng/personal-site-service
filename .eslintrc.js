736820b6f443fc879f9cb28d648292e932da597f
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-02-26 10:04:35 +0800

    change eol to lf

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     rules: {
         indent: ['error', 4],
-        'linebreak-style': ['error', 'windows'],
+        'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'never']
     }
